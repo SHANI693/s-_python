b5f00c3b1c8e8fbf4857cc866460a839ac6b223a
SHANI693 <shanibind15@gmail.com>
2026-06-20 18:18:56 +0530

    Remove input and sum calculation from code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,1 @@
-a = input("Enter the no.1 :")
-b = input("Enter the no.2 : ")
-# type=input("Enter the type of the numbers : ")
-print("The sum of the two numbers is : ", a+b) 
-if a ==int (a) and b == int (b):
-    print("The sum of the two numbers is : ", a+b)
+
