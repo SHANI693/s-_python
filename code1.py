5c9ef8691a11edf512242bab4975a80db883da05
SHANI693 <shanibind15@gmail.com>
2026-06-24 19:25:35 +0530

    Refactor code and add age validation
    Removed duplicate print statement and added age validation.

--- code1.py
+++ code1.py
@@ -5,7 +5,7 @@ print("The sum of the two numbers is : ", a+b)
 if a ==int (a) and b == int (b):
     print("The sum of the two numbers is : ", a+b)
 
-print("hello world")
+
 age = int(input("enter the age "))
 if age >= 18:
     print("your age is valid to proceed access")
